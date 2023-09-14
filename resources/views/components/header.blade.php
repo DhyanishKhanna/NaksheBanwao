cbdd29c4fb45d6124c4852067817752f3f9e4e7e
Dhyanish Khanna <dhyanish.khanna@gmail.com>
2023-09-14 14:06:12 +0530

    Adding Header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,24 @@
+<header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+        <h1 class="logo me-auto"><a href="index.html">NaksheBanwao</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                <li><a class="nav-link scrollto" href="#about">About</a></li>
+                <li><a class="nav-link scrollto" href="#services">Services</a></li>
+                <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+                <li><a class="nav-link scrollto" href="#team">Team</a></li>
+                <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+                <li><a class="nav-link scrollto" href="#faq">FAQs</a></li>
+                <li><a class="nav-link scrollto" href="#blogs">Blogs</a></li>
+                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+
+    </div>
+</header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,29 @@
 
 @section('body')
 
+<!-- Header Section -->
+<x-header />
+<!-- End Header Section -->
+
+<!-- Hero Section -->
+<section id="hero" class="d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"
+                data-aos="fade-up" data-aos-delay="200">
+                <h1> अब घर बनेगा ONLINE</h1>
+                <h2>Transforming Dreams Into Timeless Spaces</h2>
+                <div class="d-flex justify-content-center justify-content-lg-start">
+                    <a href="#contact" class="btn-get-started scrollto">Contact</a>
+                    <a href="https://www.youtube.com/@nakshebanwaoonline" class="btn-watch-video" target="_blank"><i class="bi bi-play-circle"></i><span>YouTube</span></a>
+                    <!-- <a href="https://www.youtube.com/@nakshebanwaoonline" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> -->
+                </div>
+            </div>
+            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+                <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Section -->
 @endsection
