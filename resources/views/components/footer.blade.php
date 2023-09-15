b705f86cbdc9ca59f4f7d3a3b2ff933850d82e01
Dhyanish Khanna <dhyanish.khanna@gmail.com>
2023-09-15 13:22:29 +0530

    Adding Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,66 @@
+<footer id="footer">
+
+    <div class="footer-newsletter">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <h4>Join Our NewsLetter</h4>
+                    <p>Share your email address to stay in the loop with our freshest updates.</p>
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="Subscribe">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row text-center">
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h3>NaksheBanwao</h3>
+                    <img src="assets/img/logo.png" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h4>Reach Us</h4>
+                    <p><strong>Address:</strong> Head Office, Faridabad, Haryana<br></p>
+                    <p><strong>Email:</strong> naksheBanwao@gmail.com<br></p>
+                    <p><strong>Phone:</strong> +91 97167 54525<br></p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112306.0122032872!2d77.23667292700678!3d28.402267713594224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cdc15f5a424b1%3A0xe4f50576c850e0f2!2sFaridabad%2C%20Haryana!5e0!3m2!1sen!2sin!4v1694759260755!5m2!1sen!2sin" width="80%" height="100%" style="border-radius:0.5rem;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Our Social Networks</h4>
+                    <p>Join our vibrant community and delve deeper into our world by connecting with us on our Social Media Platforms.</p>
+                    <div class="social-links mt-3">
+                        <a href="https://wa.me/919716754525" target="_blank" class="linkedin bg-success"><i
+                                class="bx bxl-whatsapp"></i></a>
+                        <a href="#" class="facebook bg-primary"><i class="bx bxl-facebook"></i></a>
+                        <a href="https://www.instagram.com/nakshebanwao" target="_blank" class="instagram"><i
+                                class="bx bxl-instagram"></i></a>
+                        <a href="https://www.youtube.com/@nakshebanwaoonline" target="_blank"
+                            class="google-plus bg-danger"><i class="bx bxl-youtube"></i></a>
+                        <a href="#" class="twitter bg-info"><i class="bx bxl-twitter"></i></a>
+                        <a href="https://in.pinterest.com/nakshebanwao" target="_blank" class="twitter bg-danger"><i
+                                class="bx bxl-pinterest"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+        <div class="copyright">
+            Copyright &copy; 2023 | <strong><span>NaksheBanwao</span></strong>
+        </div>
+        <div class="credits">
+            Designed by &nbsp;<a href="#">Dhyanish Khanna</a>
+        </div>
+    </div>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,4 +29,12 @@
     </div>
 </section>
 <!-- End Hero Section -->
+
+<!-- Footer Section -->
+<x-footer />
+<!-- End Footer Section -->
+
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
 @endsection
